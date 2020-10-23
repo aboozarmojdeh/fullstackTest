83b4a699e54dcaebd9738aaa96d1ceeef88926ea
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2020-10-22 20:21:50 -0400

    server side in progress 3

--- server/database.sql
+++ server/database.sql
@@ -2,5 +2,10 @@ CREATE DATABASE perntodofullstacktest;
 
 CREATE TABLE todo (
     todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    password VARCHAR(255),
+    description VARCHAR(255),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
--- server/index.js
+++ server/index.js
@@ -19,32 +19,60 @@ try {
 } catch (err) {
     console.error(err)
 }
-})
+});
 
 // CREATE A TODO
-app.post('/todos',async (req,res)=>{ //req.body
+app.post('/todos',async (req,res)=>{ //need req.body
 try{
-const {description}=req.body;
-const newTodo=await pool.query('INSERT INTO todo (description) VALUES($1) RETURNING *',[description]);
+const {first_name,last_name,password,description}=req.body;
+const newTodo=await pool.query('INSERT INTO todo (first_name,last_name,password,description) VALUES($1,$2,$3,$4) RETURNING *',[first_name,last_name,password,description]);
 res.json(newTodo.rows[0])
 
 }catch(err){
     console.error(err.message)
 }
-})
+});
 
 
 // GET A TODO
-app.get('/todos/:id',async (req,res)=>{
+app.get('/todos/:id',async (req,res)=>{ // need req.params
 try {
     const {id}=req.params;
     const todo=await pool.query('SELECT * FROM todo WHERE todo_id=$1',[id])
-    console.log(req.params)
+    // console.log(req.params)
     res.json(todo.rows[0])
 } catch (err) {
     console.error(err.message)
 }
-})
+});
+
+// UPDATE A TODO
+app.put('/todos/:id',async (req,res)=>{ // need req.params and req.body
+try {
+    const {id}=req.params;
+    const {first_name,last_name,password,description}=req.body;
+    const updatedDate=new Date();
+    const updateTodo=await pool.query("UPDATE todo SET first_name=$1,last_name=$2,password=$3,description=$4, updated_at=$5 WHERE todo_id=$6 RETURNING *",[first_name,last_name,password,description,updatedDate,id]);
+    res.json(updateTodo.rows[0]);
+} catch (err) {
+    console.error(err.message)
+}
+});
+
+// DELETE A TODO
+
+app.delete('/todos/:id',async (req,res)=>{ // need req.params 
+try {
+    const {id}=req.params;
+    const deleteTodo=await pool.query('DELETE FROM todo WHERE todo_id=$1',[id]);
+    res.json('Deleted')
+} catch (err) {
+    console.error(err.message)
+}
+});
+
+
+
 
 
 app.listen(5000,()=>{
